5d0e1ff0cefc98002f81c5a27849b2d9b8fca83b
nipunagg9873 <nipunagg9873@gmail.com>
2018-05-24 15:40:36 -0400

    changed port settings

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {todo}=require('./models/todo');
 var {user}=require('./models/user');
 
 var app=express();
+var port=process.env.PORT||3000;
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -51,7 +52,7 @@ app.get('/todos/:id',(req,res)=>{
 });
 
 app.listen(3000,()=>{
-  console.log('listning on port 3000');
+  console.log(`started on port ${port}`);
 });
 
 module.exports={app};
