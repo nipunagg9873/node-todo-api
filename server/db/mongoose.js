62f3b331dce69d64eeda4c9d33c3aaf043978f7f
nipunagg9873 <nipunagg9873@gmail.com>
2018-07-01 16:53:34 +0530

    fixed heroku db error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 let db={
   localhost:'mongodb://localhost:27017/TodoApp',
-  mlab:'mongodb://nipunagg9873:26842684@ds049104.mlab.com:49104/node-todo-api'
+  mlab:"mongodb://nipunagg9873:26842684@ds049104.mlab.com:49104/node-todo-api"
 }
-mongoose.connect(db.localhost||db.mlabs);
+mongoose.connect("mongodb://nipunagg9873:26842684@ds049104.mlab.com:49104/node-todo-api").catch((e)=>{
+  console.log(e);
+});
 module.exports={mongoose};
