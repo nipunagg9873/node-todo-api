76c8293c697eed5a84bc6f6abc8d5564e395525d
nipunagg9873 <nipunagg9873@gmail.com>
2018-07-15 12:52:11 +0530

    added config file

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+var env=process.env.NODE_ENV||'developement'
+
+console.log("env ****" +env)
+
+if(env==='developement'){
+  process.env.PORT=3000;
+  process.env.MONGODB_URI="mongodb://localhost:27017/TodpApp"
+}
+else if(env==='test') {
+  process.env.PORT=3000;
+  process.env.MONGODB_URI="mongodb://localhost:27017/TodpAppTest"
+}
+else {
+  process.env.MONGODB_URI="mongodb://nipunagg9873:26842684@ds049104.mlab.com:49104/node-todo-api"
+}
