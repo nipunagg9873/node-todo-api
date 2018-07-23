f59d7072370d0c4288aee3fee8dafb6a9aa67fd8
nipunagg9873 <nipunagg9873@gmail.com>
2018-07-23 19:26:15 +0530

    added login for users

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,23 @@ Userschema.statics.findByToken= function(token) {
 
 };
 
+Userschema.statics.findByCredentials= function(email,password) {
+  var User=this;
+  return User.findOne({email}).then((user)=>{
+    if(!user)
+    return Promise.reject();
+
+    return new Promise((resolve,reject)=>{
+      bcrypt.compare(password,user.password,(err,res)=>{
+        if(res)
+        resolve(user);
+        else
+        reject();
+        });
+    });
+  })
+};
+
 Userschema.pre('save',function (next) {
   var user=this;
   if(user.isModified('password')) {
